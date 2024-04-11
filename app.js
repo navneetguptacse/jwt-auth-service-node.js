265fb06e2ddfe8a259e6d5c930d31bc44cf47f25
navneetguptacse <navneetguptaid7@gmail.com>
2024-04-11 12:33:17 +0530

    init-setup

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
-const express = require('express');
-const mongoose = require('mongoose');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
 // middleware
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // view engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
+
+// environment variables
+const DB_URI = process.env.MONGO_URI;
+const PORT = process.env.PORT || 3000;
 
 // database connection
-const dbURI = 'mongodb+srv://shaun:test1234@cluster0.del96.mongodb.net/node-auth';
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
-  .then((result) => app.listen(3000))
+mongoose
+  .connect(DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then((result) =>
+    app.listen(PORT, () =>
+      console.log(`Server is running at: http://localhost:${PORT}`)
+    )
+  )
   .catch((err) => console.log(err));
 
 // routes
-app.get('/', (req, res) => res.render('home'));
-app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.get("/", (req, res) => res.render("home"));
+app.get("/smoothies", (req, res) => res.render("smoothies"));
